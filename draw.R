8dcb447f578c817ae26282701d43cd64e9f19eb6
Kim, Hirokuni <yangkookkim@gmail.com>
2017-04-09 14:51:57 +0900

    wip

--- draw.R
+++ draw.R
@@ -1,6 +1,9 @@
 # Rscript draw.R
+ind = commandArgs(trailingOnly=TRUE)[1]
+file = "coin-toss.png"
+
 (x <- read.table("/tmp/coin-toss-result.txt", header=T))
-png("mygraphic.png")
+png(file)
 plot(x$avg, type="l")
 dev.off()
-browseURL("mygraphic.png")
+browseURL(file)
